e0b11d4a36c32674f56a565add462fd0a7105481
YousifShafeeP2L <yousif@path2live.com>
2025-04-28 14:08:32 +0300

    Update Restaurant model in models.py to increase description and image field lengths for better data handling

--- main/models.py
+++ main/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Restaurant(models.Model):
     title = models.CharField(max_length=40)
-    description = models.CharField(max_length=40, null=True)
-    image = models.CharField(max_length=40, null=True)
+    description = models.CharField(max_length=200, null=True)
+    image = models.CharField(max_length=255, null=True)
     rating = models.FloatField()
 
     def __str__(self):
