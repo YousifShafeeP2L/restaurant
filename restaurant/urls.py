d5793096f00c9218c679f2bed352cd6c76b9bec6
YousifShafeeP2L <yousif@path2live.com>
2025-04-28 13:28:47 +0300

    Update Swagger URL path in restaurant/urls.py to use root path

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,6 +3,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("", SpectacularSwaggerView.as_view(url_name="schema")),
     path("", include("main.urls")),
 ]
