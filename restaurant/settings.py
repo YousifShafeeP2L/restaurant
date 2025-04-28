2b59ee9946e7f15da6d1aa742d7f49cf26436531
YousifShafeeP2L <yousif@path2live.com>
2025-04-28 11:26:32 +0300

    Add SQL mode option for database configuration in settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -68,6 +68,9 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "sql_mode": "traditional",
+        },
     }
 }
 
